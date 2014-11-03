84bd134fb45f111ab378b0df0d5a4ba22398fc5f
Information School <ischoolit@uw.edu>
2014-11-03 12:07:55 -0800

    end of class

--- js/app.js
+++ js/app.js
@@ -9,4 +9,16 @@
 angular.module('MovieApp', [])
     .controller('MoviesController', function($scope) {
         $scope.movies = movies;
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function(colName) {
+            if($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortReverse = false;
+                $scope.sortCol = colName;
+            }
+        };
     });
